027e0387c0e1a6bbdea0917ed43ba09fb164585f
jrajerison <rajerison@bocasay.com>
2020-03-14 14:37:06 +0300

    fix header emplacement

--- src/pages/VieQuotidien/PhoneUtils.tsx
+++ src/pages/VieQuotidien/PhoneUtils.tsx
@@ -30,8 +30,8 @@ export default class PhoneUtils extends React.Component<any, any> {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
             <IonPage>
+                <Header/>
                 <IonContent>
-                    <Header/>
                     <FabButton/>
                     <IonList lines={"full"}>
                         {
